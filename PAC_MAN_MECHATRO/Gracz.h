534cf89077900de99b13eec50b166d19ff14448f
RonaldoPW <czlowiek.z.polski02@gmail.com>
2026-05-29 17:33:33 +0200

    Add files via upload

--- PAC_MAN_MECHATRO/Gracz.h
+++ PAC_MAN_MECHATRO/Gracz.h
@@ -1,15 +1,20 @@
 #ifndef GRACZ_H
 #define GRACZ_H
-#include "elementy_gry.h"
-#endif // GRACZ_H
 
-class Gracz : Elementygry{
+#include "elementy_gry.h"
+
+using namespace std;
+
+class Gracz : public ElementyGry {
 private:
     int m_lives;
     int m_score;
+    int m_id;
 
 public:
-    Gracz(int x, int y, int width, int height, int speed, int startingLives = 3);
+    Gracz(int x, int y, int width, int height, int speed, int id, int startingLives = 3);
     void update();
-
+    int getId() const;
 };
+
+#endif
